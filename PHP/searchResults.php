dfe8341c34cff8760b8455ef43f90ac654fd251a
H2O2 <hcy1111hcy@sina.com>
2023-06-01 02:22:04 +0800

    ✨ feat(search): search basic func & BGM

---
+++ PHP/searchResults.php
@@ -0,0 +1,50 @@
+<?php
+$host = 'localhost:3306';
+$db = 'oasd';
+$user = 'root';
+$pass = 'gansui';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+$keyword = $_GET['name'] ?? $_GET['author'];
+$field = array_key_exists('name', $_GET) ? 'name' : 'author';
+$sortField = 'price';
+$sortOrder = 'ASC';
+
+if ($_GET['sort']) {
+    switch ($_GET['sort']) {
+        case 'priceAsc':$sortField = 'price';
+            $sortOrder = 'ASC';
+            break;
+        case 'priceDesc':$sortField = 'price';
+            $sortOrder = 'DESC';
+            break;
+        case 'yearAsc':$sortField = 'year';
+            $sortOrder = 'ASC';
+            break;
+        case 'yearDesc':$sortField = 'year';
+            $sortOrder = 'DESC';
+            break;
+    }
+}
+
+$page = $_GET['page'] ?? 1;
+$resultsPerPage = 5;
+$offset = ($page - 1) * $resultsPerPage;
+
+$stmt = $pdo->prepare("SELECT * FROM artwork WHERE $field LIKE ? ORDER BY $sortField $sortOrder LIMIT $resultsPerPage OFFSET $offset");
+$stmt->execute(["%$keyword%"]);
+$artworks = $stmt->fetchAll();
+
+$stmt = $pdo->prepare("SELECT COUNT(*) FROM artwork WHERE $field LIKE ?");
+$stmt->execute(["%$keyword%"]);
+$totalCount = $stmt->fetchColumn();
+
+echo json_encode(['artworks' => $artworks, 'totalCount' => $totalCount]);
---
+++ js/search.js
@@ -0,0 +1,116 @@
+// async function fetchResults() {
+//     const searchType = document.getElementById('searchType').value;
+//     const searchTerm = document.getElementById('searchBox').value;
+//     const response = await fetch(`http://localhost:3000/PHP/search.php?${searchType}=${searchTerm}`);
+//     const results = await response.json();
+
+//     const resultContainer = document.getElementById('resultContainer');
+//     resultContainer.innerHTML = '';
+
+//     results.forEach(result => {
+//         const card = document.createElement('div');
+//         card.classList.add('resultCard');
+
+//         const img = document.createElement('img');
+//         img.src = `/PHP/${result.image}`;
+//         card.appendChild(img);
+
+//         const name = document.createElement('h2');
+//         name.textContent = result.name;
+//         card.appendChild(name);
+
+//         const author = document.createElement('h3');
+//         author.textContent = result.author;
+//         card.appendChild(author);
+
+//         const price = document.createElement('p');
+//         price.textContent = `Price: ${result.price}`;
+//         card.appendChild(price);
+
+//         const description = document.createElement('p');
+//         description.textContent = result.description;
+//         card.appendChild(description);
+
+//         resultContainer.appendChild(card);
+//     });
+// }
+
+// document.getElementById('searchButton').addEventListener('click', fetchResults);
+// fetchResults();
+
+let currentPage = 1;
+const resultsPerPage = 5;
+
+async function fetchResults() {
+    const searchType = document.getElementById('searchType').value;
+    const searchTerm = document.getElementById('searchBox').value || '%';
+    const sortType = document.getElementById('sortType').value;
+
+    const response = await fetch(`http://localhost:3000/PHP/searchResults.php?${searchType}=${searchTerm}&sort=${sortType}&page=${currentPage}`);
+    const results = await response.json();
+
+    displayResults(results.artworks);
+    displayResultSummary(results.totalCount);
+    displayPagination(results.totalCount);
+}
+
+function displayResults(artworks) {
+    const container = document.getElementById('resultContainer');
+    container.innerHTML = '';
+
+    if (artworks.length === 0) {
+        container.innerHTML = 'No results found';
+        return;
+    }
+
+    for (let artwork of artworks) {
+        let card = document.createElement('div');
+        card.className = 'resultCard';
+
+        let image = document.createElement('img');
+        image.src = '/PHP/' + artwork.image;
+        image.alt = artwork.name;
+        card.appendChild(image);
+
+        let name = document.createElement('h2');
+        name.textContent = artwork.name;
+        card.appendChild(name);
+
+        let author = document.createElement('h3');
+        author.textContent = artwork.author;
+        card.appendChild(author);
+
+        let year = document.createElement('p'); // 新增显示年份的元素
+        year.textContent = 'Year: ' + artwork.year;
+        card.appendChild(year);
+
+        let price = document.createElement('p');
+        price.textContent = 'Price: ' + artwork.price;
+        card.appendChild(price);
+
+        card.addEventListener('click', function () {
+            window.location.href = 'artworkDetail.html?id=' + artwork.id;
+        });
+
+        container.appendChild(card);
+    }
+}
+
+
+
+function displayResultSummary(totalCount) {
+    document.getElementById('resultSummary').textContent = `Found ${totalCount} results`;
+}
+
+function displayPagination(totalCount) {
+    const totalPages = Math.ceil(totalCount / resultsPerPage);
+    document.getElementById('pageInfo').textContent = `Page ${currentPage} of ${totalPages}`;
+    document.getElementById('prevButton').disabled = currentPage === 1;
+    document.getElementById('nextButton').disabled = currentPage === totalPages;
+}
+
+document.getElementById('searchButton').addEventListener('click', fetchResults);
+document.getElementById('prevButton').addEventListener('click', () => { currentPage--; fetchResults(); });
+document.getElementById('nextButton').addEventListener('click', () => { currentPage++; fetchResults(); });
+
+window.onload = fetchResults;
